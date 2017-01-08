e54b80b50dc81b6c42b8ac4fcc993c0a6457f0fa
foxjazz <foxr4@foxjazz.net>
2017-01-07 21:21:09 -0700

    changed err handling

--- anonymous-routes.js
+++ anonymous-routes.js
@@ -15,30 +15,37 @@ app.get('/couchDataAll', function(req, res) {
     // let info = db.info();
     // let dbs = db.databases();
     console.log('getting data');
-    var test = db.all(function(re, rs) {
-        var gots = JSON.parse(rs);
-        for (i = 0; i < gots.length; i++) {
-            db.get(gots[i].id, function(err, doc) {
-                if (err.length > 0) {
-                    console.log(err);
-                } else {
-                    members.push(doc);
-                }
-            })
+    var test = db.all(function(err, rs) {
+        if (err) {
+            console.dir(err);
+        } else {
+
+            var gots = JSON.parse(rs);
+            for (i = 0; i < gots.length; i++) {
+                db.get(gots[i].id, function(err, doc) {
+                    if (err) {
+                        console.dir(err);
+                    } else {
+                        members.push(doc);
+                    }
+                });
+            }
+            res.status(200).send(JSON.stringify(members));
         }
-        res.status(200).send(JSON.stringify(members));
+
     })
 
 });
 app.post('/couchDelete', function(req, res) {
     var id = req.body._id;
     db.remove(req.body._id, req.body._rev, function(err, r) {
-        if (err.length > 0) {
+        if (err) {
+            console.dir(err);
             res.status(500).send(err);
-            console.log(err);
+
         } else {
             res.status(200).send(r)
-            console.log("deleted "+ id);
+            console.log("deleted " + id);
         }
     });
 });
@@ -52,7 +59,7 @@ app.post('/couchSave', function(req, res1) {
     console.log('saving ' + req.body);
     db.save(req.body._id, req.body, function(err, r1) {
         if (err) {
-            console.log(err.json());
+            console.dir(err);
             res1.status(500).send(err);
         } else {
             res1.status(200).send(r1);

ba2ff3c06980e9087caf1c96ea06474bd0ea1947
foxjazz <foxr4@foxjazz.net>
2017-02-03 18:44:05 -0700

    re-arranged rules

--- processing/rules.ts
+++ processing/rules.ts
@@ -2,11 +2,11 @@
 
 export let rules = [
   {TermInMonths: 0, Amount: 0, MembershipType: "Not Active", expDate: "1/1/2100"},
-  {TermInMonths: 1, Amount: 5, MembershipType: "Regular", expDate: "12/1/2018"},
-  {TermInMonths: 1, Amount: 15, MembershipType: "Family", expDate: "12/1/2018"},
   {TermInMonths: 12, Amount: 60, MembershipType: "Regular", expDate: "12/1/2018"},
   {TermInMonths: 12, Amount: 45, MembershipType: "Regular", expDate: "12/1/2016"},
   {TermInMonths: 12, Amount: 75, MembershipType: "Family", expDate: "1/1/2100"},
+  {TermInMonths: 1, Amount: 5, MembershipType: "Regular", expDate: "12/1/2018"},
+  {TermInMonths: 1, Amount: 15, MembershipType: "Family", expDate: "12/1/2018"},
   {TermInMonths: 12, Amount: 300, MembershipType: "Supporter", expDate: "1/1/2100"},
   {TermInMonths: 12, Amount: 600, MembershipType: "Sustainer", expDate: "1/1/2100"},
   {TermInMonths: 12, Amount: 99999, MembershipType: "VIP", expDate: "1/1/2100"},
